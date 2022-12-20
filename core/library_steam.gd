d2212347680112be558d675aafa6ccccb553d308
William Edwards <shadowapex@gmail.com>
2022-12-19 18:35:22 -0800

    Add caching of apps

--- core/library_steam.gd
+++ core/library_steam.gd
@@ -3,16 +3,12 @@ extends Library
 # Other interesting commands
 # steamcmd +login shadowapex +apps_installed +quit
 
-@export var use_caching: bool = true
-
 const SteamClient := preload("res://plugins/steam/core/steam/client.gd")
 const _apps_cache_file: String = "apps.json"
-const _app_info_cache_file: String = "app_info.json"
-var _steam_dir: String = "/".join([OS.get_environment("HOME"), ".steam"])
-var _steam_libraryfolders: String = _steam_dir + "/steam/steamapps/libraryfolders.vdf"
+var steam_dir: String = "/".join([OS.get_environment("HOME"), ".steam"])
+var steam_libraryfolders: String = steam_dir + "/steam/steamapps/libraryfolders.vdf"
 
 @onready var steam_client: SteamClient = get_tree().get_first_node_in_group("steam_client")
-@onready var library_manager: LibraryManager = get_tree().get_first_node_in_group("library_manager")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,12 +20,43 @@ func _ready() -> void:
 
 # Re-load our library when we've logged in
 func _on_logged_in():
-	library_manager.reload_library()
+	# Upon login, fetch the user's library without loading it from cache and 
+	# reconcile it with the library manager.
+	logger.debug("Logged in. Updating library cache from Steam.")
+	var items: Array = await _load_library(Cache.FLAGS.SAVE)
+	for i in items:
+		var item: LibraryLaunchItem = i
+		# TODO: add/remove instead of reloading the whole library
+		if not library_manager.has_app(item.name):
+			logger.debug("App {0} was not loaded. Re-loading library from updated cache.".format([item.name]))
+			library_manager.reload_library()
+			return
+			
+	logger.debug("Library is already up-to-date")
 
 
-# Return a list of installed steam apps
-# TODO: Figure out Steam auth
+# Return a list of installed steam apps. Called by the LibraryManager.
 func get_library_launch_items() -> Array:
+	return await _load_library(Cache.FLAGS.LOAD|Cache.FLAGS.SAVE)
+
+
+# Return a list of installed steam apps. Optionally caching flags can be passed to
+# determine caching behavior.
+# Example:
+#   _load_library(Cache.FLAGS.LOAD|Cache.FLAGS.SAVE)
+func _load_library(caching_flags: int = Cache.FLAGS.LOAD|Cache.FLAGS.SAVE) -> Array:
+	# Check to see if our library was cached. If it was, return the cached
+	# items.
+	if caching_flags & Cache.FLAGS.LOAD and Cache.is_cached(_cache_dir, _apps_cache_file):
+		var json_items = Cache.get_json(_cache_dir, _apps_cache_file)
+		if json_items != null:
+			logger.debug("Available apps exist in cache. Using cache.")
+			var items := []
+			for i in json_items:
+				var item: Dictionary = i
+				items.append(LibraryLaunchItem.from_dict(item))
+			return items
+	
 	# Wait for the steam client if it's not ready
 	if not steam_client.client_ready:
 		logger.info("Steam client is not ready yet.")
@@ -45,7 +72,7 @@ func get_library_launch_items() -> Array:
 	var app_info: Dictionary = await _get_app_info(app_ids)
 
 	# Load the local Steam library folders
-	var library_folders: Dictionary = await _load_library_folders(_steam_libraryfolders)
+	var library_folders: Dictionary = await _load_library_folders(steam_libraryfolders)
 
 	# Generate launch items for each game
 	var items := []
@@ -58,6 +85,16 @@ func get_library_launch_items() -> Array:
 		item.tags = ["steam"]
 		item.installed = _is_installed(library_folders, item.provider_app_id)
 		items.append(item)
+
+	# Cache the discovered apps
+	if caching_flags & Cache.FLAGS.SAVE:
+		logger.debug("Saving available apps to cache.")
+		var json_items := []
+		for i in items:
+			var item: LibraryLaunchItem = i
+			json_items.append(item.to_dict())
+		if Cache.save_json(_cache_dir, _apps_cache_file, json_items) != OK:
+			logger.warn("Unable to save Steam apps cache")
 
 	return items
 
