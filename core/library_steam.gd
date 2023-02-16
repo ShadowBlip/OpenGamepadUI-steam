13a4727f0194fdf68e83802f45bab13969ba899b
William Edwards <shadowapex@gmail.com>
2023-02-16 00:25:51 -0800

    Implement install, update, and uninstall

--- core/library_steam.gd
+++ core/library_steam.gd
@@ -19,6 +19,95 @@ func _ready() -> void:
 	steam.logged_in.connect(_on_logged_in)
 
 
+# Return a list of installed steam apps. Called by the LibraryManager.
+func get_library_launch_items() -> Array[LibraryLaunchItem]:
+	return await _load_library(Cache.FLAGS.LOAD | Cache.FLAGS.SAVE)
+
+
+# Installs the given library item.
+func install(item: LibraryLaunchItem) -> void:
+	# Start the install
+	var app_id := item.provider_app_id
+	logger.info("Installing " + item.name + " with app ID: " + app_id)
+	steam.install(app_id)
+
+	# Connect to progress updates
+	var on_progress := func(id: String, bytes_cur: int, bytes_total: int):
+		if id != app_id:
+			return
+		logger.info("Install progressing: " + str(bytes_cur) + "/" + str(bytes_total))
+		var progress: float = float(bytes_cur) / float(bytes_total)
+		if bytes_total == 0:
+			progress = 0
+		install_progressed.emit(item, progress)
+	steam.install_progressed.connect(on_progress)
+
+	# Wait for the app_installed signal
+	var success := false
+	var installed_app := ""
+	while installed_app != app_id:
+		var results = await steam.app_installed
+		installed_app = results[0]
+		success = results[1]
+	install_completed.emit(item, success)
+	logger.info("Install of " + item.name + " completed with status: " + str(success))
+
+	# Disconnect from progress updates 
+	steam.install_progressed.disconnect(on_progress)
+
+
+# Updates the given library item.
+func update(item: LibraryLaunchItem) -> void:
+	# Start the install
+	var app_id := item.provider_app_id
+	logger.info("Updating " + item.name + " with app ID: " + app_id)
+	steam.install(app_id)
+
+	# Connect to progress updates
+	var on_progress := func(id: String, bytes_cur: int, bytes_total: int):
+		if id != app_id:
+			return
+		logger.info("Update progressing: " + str(bytes_cur) + "/" + str(bytes_total))
+		install_progressed.emit(item, float(bytes_total)/float(bytes_cur))
+	steam.install_progressed.connect(on_progress)
+
+	# Wait for the app_updated signal
+	var success := false
+	var installed_app := ""
+	while installed_app != app_id:
+		var results = await steam.app_updated
+		installed_app = results[0]
+		success = results[1]
+	update_completed.emit(item, success)
+	logger.info("Update of " + item.name + " completed with status: " + str(success))
+
+	# Disconnect from progress updates 
+	steam.install_progressed.disconnect(on_progress)
+
+
+# Uninstalls the given library item.
+func uninstall(item: LibraryLaunchItem) -> void:
+	# Start the uninstall
+	var app_id := item.provider_app_id
+	logger.info("Uninstalling " + item.name + " with app ID: " + app_id)
+	steam.uninstall(app_id)
+
+	# Wait for the app_uninstalled signal
+	var success := false
+	var installed_app := ""
+	while installed_app != app_id:
+		var results = await steam.app_uninstalled
+		installed_app = results[0]
+		success = results[1]
+	uninstall_completed.emit(item, success)
+	logger.info("Uninstall of " + item.name + " completed with status: " + str(success))
+
+
+# Should return true if the given library item has an update available
+func has_update(item: LibraryLaunchItem) -> bool:
+	return false
+
+
 # Re-load our library when we've logged in
 func _on_logged_in(status: SteamClient.LOGIN_STATUS):
 	if status != SteamClient.LOGIN_STATUS.OK:
@@ -38,11 +127,6 @@ func _on_logged_in(status: SteamClient.LOGIN_STATUS):
 			return
 
 	logger.debug("Library is already up-to-date")
-
-
-# Return a list of installed steam apps. Called by the LibraryManager.
-func get_library_launch_items() -> Array[LibraryLaunchItem]:
-	return await _load_library(Cache.FLAGS.LOAD | Cache.FLAGS.SAVE)
 
 
 # Return a list of installed steam apps. Optionally caching flags can be passed to
